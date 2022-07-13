678f4ed96e841069f9c69d25a8344ac444eabebf
ditdili <aliyevmaqsud@gmail.com>
2022-07-13 05:12:31 -0500

    feat: completes challenge 6, adds policyholder view

--- src/components/PolicyholdersView/PolicyholdersView.tsx
+++ src/components/PolicyholdersView/PolicyholdersView.tsx
@@ -1,6 +1,8 @@
 import { Box } from '@mui/material';
 import { useQuery } from 'react-query';
 import { getPolicyholders } from '../../utils/apiClient';
+import { policyholdersKeyValueGenerator } from '../../utils/policyholdersHelpers';
+import InfoTable from '../InfoTable';
 
 const PolicyholdersView = () => {
   const { isLoading, isError, error, data } = useQuery(
@@ -11,9 +13,15 @@ const PolicyholdersView = () => {
   if (isLoading) return <Box>Loading...</Box>;
   if (isError && error instanceof Error) return <Box>{error.message}</Box>;
 
-  console.log(data);
+  const policyHoldersKeyValues = policyholdersKeyValueGenerator(
+    data.policyHolders
+  );
 
-  return null;
+  return (
+    <Box sx={{ textAlign: 'center' }}>
+      <InfoTable header="Policy Holder" rows={policyHoldersKeyValues[0]} />
+    </Box>
+  );
 };
 
 export default PolicyholdersView;
---
+++ src/utils/policyholdersHelpers.ts
@@ -0,0 +1,64 @@
+type Address = {
+  line1: string;
+  line2: string | undefined;
+  city: string;
+  state: string;
+  postalCode: string;
+};
+
+export type PolicyHolder = {
+  name: string;
+  age: number;
+  address: Address;
+  phoneNumber: string;
+  isPrimary: boolean;
+};
+
+const keyValueDict: Record<string, string> = {
+  name: 'Name',
+  age: 'Age',
+  address: 'Address',
+  phoneNumber: 'Phone number',
+  isPrimary: 'Primary policyholder?',
+};
+type Values = {
+  address: string;
+  isPrimary: string;
+};
+
+type KeyValueGeneratorReturn = {
+  key: string;
+  value: string | number;
+};
+
+const keyValueGenerator = (
+  policyHolder: PolicyHolder,
+  keyValue: string
+): KeyValueGeneratorReturn => {
+  const value = policyHolder[keyValue as keyof PolicyHolder];
+
+  const values: Values = {
+    address:
+      typeof value === 'object'
+        ? `${value.line1} ${value.line2}, ${value.city}, ${value.state} ${value.postalCode}`
+        : '',
+    isPrimary: value.toString(),
+  };
+
+  return {
+    key: keyValueDict[keyValue],
+    value: values[keyValue as keyof Values] || (value as string),
+  };
+};
+
+export const policyholdersKeyValueGenerator = (
+  policyHoldersArray: PolicyHolder[]
+) => {
+  const result = policyHoldersArray.map((policyHolder: PolicyHolder) => {
+    return Object.keys(policyHolder).map((keyValue: string) => {
+      return keyValueGenerator(policyHolder, keyValue);
+    });
+  });
+
+  return result;
+};
